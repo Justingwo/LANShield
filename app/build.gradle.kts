705055fd1c45766176941a4d6467f97dafeccb9b
Jeroen Robben <jeroen@robben.io>
2026-09-07 22:50:16 +0200

    v0.99 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.distrinet.lanshield"
         minSdk = 29
         targetSdk = 37
-        versionCode = 23
-        versionName = "0.98"
+        versionCode = 24
+        versionName = "0.99"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         // Run each instrumented test in its own process with cleared app data/permissions, so that
